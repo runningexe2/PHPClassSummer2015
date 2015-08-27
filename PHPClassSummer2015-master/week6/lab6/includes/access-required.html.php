d0ece6f3e27f008284c4462fa41f446e3a0227cb
001246352 <001246352@N203A1.labs.neit.edu>
2015-08-26 21:45:57 -0400

    Merge origin/master
    Conflicts:
    	nbproject/project.xml

--- PHPClassSummer2015-master/week6/lab6/includes/access-required.html.php
+++ PHPClassSummer2015-master/week6/lab6/includes/access-required.html.php
@@ -1,3 +1,3 @@
 <?php if ( !isset($_SESSION['isValidUser']) || $_SESSION['isValidUser'] !== true ) : ?>
-    <p><a href="../adminIndex.php">Login</a></p>
+<p><a href="../admin/adminIndex.php">Login</a></p>
 <?php die('Access Denied '); endif;  ?>
--- PHPClassSummer2015-master/week6/lab6/includes/admin-links.html.php
+++ PHPClassSummer2015-master/week6/lab6/includes/admin-links.html.php
@@ -1,6 +1,7 @@
 <p>You are logged in</p>
 <h2>Links</h2>
+
 <ul>
-    <li><a href="category/">Category Admin</a></li>
-    <li><a href="products/">Products Admin</a></li>
+    <li><a href="../admin/categories/create.php">Category Admin</a></li>
+    <li><a href="../admin/products/create.php">Products Admin</a></li>
 </ul>
